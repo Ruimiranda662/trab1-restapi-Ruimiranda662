f862b14601ba8c465a22fabefbb0fdb42f52b566
Rui Miranda <rui.miranda@ipvc.pt>
2025-07-13 17:19:24 +0100

    aaaa

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const alunosRouter = require('./routes/alunos');
+const alunosRouter = require('./routes/alunosrotas');
 
 const app = express();
 app.use(cors());
